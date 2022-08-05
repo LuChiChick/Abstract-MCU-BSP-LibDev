f24106617c602d84afe11ee8dfcac04efe51f614
LuChiChick <1084116302@qq.com>
2022-08-05 20:20:36 +0800

    移除BaseIO相关，工程解耦

--- User/Implements/MSP432/Inc/MSP432_BaseIO.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __CUS__MSP432_BASEIO__
-#define __CUS__MSP432_BASEIO__
-
-#include "BaseIO.hpp"
-#include "MSP432_GPIO_Wire.hpp"
-
-#endif
--- User/Implements/STM32_HAL/Inc/STM32_BaseIO.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __CUS__STM32_HAL_BASEIO__
-#define __CUS__STM32_HAL_BASEIO__
-
-#include "BaseIO.hpp"
-#include "STM32_GPIO_Wire.hpp"
-
-#endif
